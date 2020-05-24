8751e69617fdce88513b46d2795d56e44ef01edd
Guido Alpern <guidoalpern@gmail.com>
2020-05-24 17:16:35 -0300

    Fixed favicon stuff

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # 2' ENERC Se Proyecta
 Sitio web para la segunda edición del festival "ENERC Se proyecta" en su versión online.
 
-React + Material UI
+Made with React + Material UI
 
 ## CLI Commands
 
 ### npm start
 Para iniciar la app en modo desarrollo, se abrirá en http://localhost:3000
+
+## ToDo
+  * Agregar Favicon y thumbnails
--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import {Helmet} from 'react-helmet';
 import {Header} from './components/header/header.component';
-import {Section} from './components/section/section.component'
+import {Section} from './components/section/section.component';
+import Favicon from './assets/images/favicon.png';
 
 import './scss/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Helmet title="" />
       <Helmet>
         <meta charSet="utf-8" />
-        <title>2' ENERC SE PROYECTA</title>
+        <title>2' ENERC SE PROYECTA | Edición Online | 2020</title>
         <link rel="canonical" href="https://enercseproyecta.com.ar" />
         <meta name="description" content="Festival organizado integramente por alumnes de la Escuela Nacional de Experimentación y Realización Cinematográfica." />
+        <link rel="icon" type="image/png" href={Favicon} sizes="16x16" />
       </Helmet>
       <Header />
-      <Section maxWidth="false">
+      <Section maxWidth="xl">
         <div className="logo">
           <span>2'</span>
           <span>ENERC</span>
