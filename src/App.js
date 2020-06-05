e7b4db3ffd2a1f9473483c6bba2ea01204da679c
Guido Alpern <guidoalpern@gmail.com>
2020-06-04 23:03:33 -0300

    Added instagram icon for temporary site

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import {Helmet} from 'react-helmet';
 import {Header} from './components/header/header.component';
@@ -40,23 +41,12 @@ class App extends Component {
             <span>PROYECTA</span>
           </div>
         </Section>
-        <Section className="border-bottom" maxWidth="lg">
-          <h2>Ver</h2>
-          <div className="streaming-player">
-            <iframe width="100%" height="600" title="streaming-player" className="streaming-player" src="https://www.youtube.com/embed/E4gjFgY0kVQ?rel=0&showinfo=0&modestbranding=1" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <Section maxWidth="xl">
+          <div className="logo">
+            <span>6 Y 7</span>
+            <span>13 Y 14</span>
+            <span>DE JUNIO</span>
           </div>
-        </Section>
-        <Section className="border-bottom" maxWidth="lg">
-          <h2>Chat</h2>
-        </Section>
-        <Section className="border-bottom" maxWidth="lg">
-          <h2>Programación</h2>
-        </Section>
-        <Section className="border-bottom" maxWidth="lg">
-          <h2>Sponsors</h2>
-        </Section>
-        <Section className="border-bottom" maxWidth="lg">
-          <h2>Equipo</h2>
         </Section>
       </div>
     );
--- src/components/header-menu/header-menu.component.jsx
+++ src/components/header-menu/header-menu.component.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 import './header-menu.styles.css';
 
@@ -29,10 +28,10 @@ export default function HeaderMenu() {
   return (
     <div className={classes.root}>
       <AppBar position="static" style={{ background: '#FFF' }}  elevation={0}>
-        <Toolbar>
-          <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon fontSize="large" />
-          </IconButton>
+        <Toolbar width="100%" style={{ justifyContent: 'flex-end' }}>
+        <a href="https://instagram.com/enercseproyecta" rel="noopener noreferrer" target="_blank">
+          <InstagramIcon fontSize="large" style={{ color: '#000' }} align="right" />
+        </a>
         </Toolbar>
       </AppBar>
     </div>
